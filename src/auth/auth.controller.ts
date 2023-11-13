aaf1f47cfb788d2110add96e60577af78f6164c9
Dutus244 <duynguyen24th@gmail.com>
2023-11-13 23:56:05 +0700

    Revert

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Post,
+  UseGuards,
+  Request,
 } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { AuthGuard } from './auth.guard'
 
 @Controller('')
 export class AuthController {
@@ -15,4 +19,10 @@ export class AuthController {
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password)
   }
+
+//   @UseGuards(AuthGuard)
+//   @Get('profile')
+//   getProfile(@Request() req) {
+//     return req.user
+//   }
 }
