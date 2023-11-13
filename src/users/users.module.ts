384ffa476c7ca974e006d459d888f72017505208
Samuel <20127610@student.hcmus.edu.vn>
2023-11-13 11:33:32 +0700

    fix

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { UsersController } from './users.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Users } from 'src/entity/users.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Users])],
   providers: [UsersService],
   exports: [UsersService],
   controllers: [UsersController],
