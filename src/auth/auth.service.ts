4e441d1011bbafc38f9ce3b901f0a1013d7ab07a
Dutus244 <duynguyen24th@gmail.com>
2023-11-14 17:27:02 +0700

    Fix signin api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,15 +5,20 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private userService: UsersService,
-        private jwtService: JwtService
-    ) { }
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService
+  ) { }
 
     async signIn(username, password): Promise<any> {
         const user = await this.userService.findOneByUserName(username);
 
-        const isMatch = await bcrypt.compare(password, user?.password.toString());
+    if (!user) {
+      // User not found, handle accordingly
+      throw new UnauthorizedException()
+    }
+
+    const isMatch = await bcrypt.compareSync(password, user?.password.toString())
 
         if (isMatch) {
             const payload = { sub: user.UserID, username: user.username }
